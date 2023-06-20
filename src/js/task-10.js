79d1ea953fb5cb91848adda7aa62014ab8582b12
Liudmila-Ridko <125599487+liussi@users.noreply.github.com>
2023-06-20 22:11:03 +0300

    поправки 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -6,7 +6,7 @@ const amountEl = document.querySelector('#controls input')
 
 
 function createBoxes(amount) {
-  for(var i = 0; i < amount; i+= 1) {
+  for(let i = 0; i < amount; i+= 1) {
     let elem = document.createElement('div');
     
     elem.style.width = 30 + (10 * i)+'px';
